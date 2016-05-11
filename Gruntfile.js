e1136bd0fd5d63aafe8b7e139c6b59911023b3a0
Niels Lindberg <eighthourcream@gmail.com>
2016-05-11 15:58:51 +0200

    Finished adding recipes

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        responsive_images: {
+            create_images: {
+                options: {
+                    engine: 'im',
+                    newFilesOnly: true,
+                    sizes: [
+                        { width: 200 },
+                        { width: 300 },
+                        { width: 400 }
+                    ]
+                },
+                files: [{
+                    expand: true,
+                    src: ['**.{jpeg,JPG,jpg,gif,png}'],
+                    cwd: "development/images/",
+                    dest: "production/images/"
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.registerTask('default', ['responsive_images']);
+};
